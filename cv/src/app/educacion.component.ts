5f9ad1fba336888e1851193c66e6519ac1632663
JuanDi37 <142911123+JuanDi37@users.noreply.github.com>
2025-10-13 18:37:24 -0600

    v1

--- cv/src/app/educacion.component.ts
+++ cv/src/app/educacion.component.ts
@@ -1,22 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ApiService } from './services/api.service'; 
 
 @Component({
   selector: 'educacion',
   standalone: true,
+  imports: [CommonModule],
   template: `
 <section id="educacion">
   <h2>Educación</h2>
-  <table>
+
+  <table *ngIf="education.length > 0; else loading">
     <thead>
       <tr><th>Año</th><th>Institución</th><th>Título</th></tr>
     </thead>
     <tbody>
-      <tr><td>2022</td><td>Colegio Valle Verde (High School)</td><td>Bachillerato</td></tr>
-      <tr><td>2023 - 2026</td><td>Universidad Francisco Marroquín</td><td>Licenciatura en Computer Science (en curso)</td></tr>
-      <tr><td>2024</td><td>Universidad Francisco Marroquín</td><td>Scrum and Agile Methodologies</td></tr>
+      <tr *ngFor="let e of education">
+        <td>{{ e.startYear }} - {{ e.endYear }}</td>
+        <td>{{ e.school }}</td>
+        <td>{{ e.degree }}</td>
+      </tr>
     </tbody>
   </table>
+
+  <ng-template #loading>
+    <p>Cargando educación...</p>
+  </ng-template>
 </section>
   `
 })
-export class EducacionComponent {}
+export class EducacionComponent implements OnInit {
+  education: any[] = [];
+
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.api.getEducation().subscribe({
+      next: (data) => (this.education = data),
+      error: (err) => console.error('Error al cargar educación:', err)
+    });
+  }
+}
